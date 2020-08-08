f934dbbc2c35912c3c2a79cdd74ceaad0c0cc439
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-08 22:55:22 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create({ first_name: "Tylor", last_name: "Swift"})
+
+# Student.create([{ first_name: "Rick", last_name: "Sanchez" }, { first_name: "Morty", last_name: "Smith" }])
